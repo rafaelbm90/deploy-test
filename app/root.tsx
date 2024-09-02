4160a3161b6884399f25bc684e961fc9193ea4ea
Rafael Moraes <rafaelbastosmoraes@gmail.com>
2024-09-02 16:17:19 -0300

    moved styles to public folder

--- app/root.tsx
+++ app/root.tsx
@@ -10,9 +10,11 @@ import { LinksFunction } from "@remix-run/node";
 
 import NavBar from "./components/NavBar";
 
-import styles from "./styles/main.css";
+// import styles from "./styles/main.css";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: "/styles/main.css" },
+];
 
 export default function App() {
   return (
